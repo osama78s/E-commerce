27a6ec83533f04adbdf0e68d0c95a7fa993e0db4
osama <osamasaif242@gmail.com>
2024-08-30 17:18:21 +0300

    first

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
     <Provider store={store}>
       <App />
     </Provider>
