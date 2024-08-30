7c6ef18fc7ea457523f8b87655efe0b50d87404c
osama <osamasaif242@gmail.com>
2024-08-30 17:34:07 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import store from './Redux/Store/Store.jsx'
 import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/'>
+    <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
